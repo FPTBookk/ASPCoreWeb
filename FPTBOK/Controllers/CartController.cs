9946ab7c58b695a22c45590239642fbb843e0731
TruongDuongHongPhuoc <121841902+TruongDuongHongPhuoc@users.noreply.github.com>
2023-10-25 22:51:26 +0700

    comit

---
+++ FPTBOK/Controllers/CartController.cs
@@ -0,0 +1,167 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FPTBOK.Models;
+
+namespace FPTBOK.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly FPTDBTContext _context;
+
+        public CartController(FPTDBTContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Cart
+        public async Task<IActionResult> Index()
+        {
+            var fPTDBTContext = _context.Cart.Include(c => c.Order);
+            return View(await fPTDBTContext.ToListAsync());
+        }
+
+        // GET: Cart/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Cart == null)
+            {
+                return NotFound();
+            }
+
+            var cart = await _context.Cart
+                .Include(c => c.Order)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cart == null)
+            {
+                return NotFound();
+            }
+
+            return View(cart);
+        }
+
+        // GET: Cart/Create
+        public IActionResult Create()
+        {
+            ViewData["OrderId"] = new SelectList(_context.Set<OrderBook>(), "OrderId", "OrderId");
+            return View();
+        }
+
+        // POST: Cart/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,BookId,Quantity,TotalPrice,OrderId")] Cart cart)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cart);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["OrderId"] = new SelectList(_context.Set<OrderBook>(), "OrderId", "OrderId", cart.OrderId);
+            return View(cart);
+        }
+
+        // GET: Cart/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Cart == null)
+            {
+                return NotFound();
+            }
+
+            var cart = await _context.Cart.FindAsync(id);
+            if (cart == null)
+            {
+                return NotFound();
+            }
+            ViewData["OrderId"] = new SelectList(_context.Set<OrderBook>(), "OrderId", "OrderId", cart.OrderId);
+            return View(cart);
+        }
+
+        // POST: Cart/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,BookId,Quantity,TotalPrice,OrderId")] Cart cart)
+        {
+            if (id != cart.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cart);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CartExists(cart.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["OrderId"] = new SelectList(_context.Set<OrderBook>(), "OrderId", "OrderId", cart.OrderId);
+            return View(cart);
+        }
+
+        // GET: Cart/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Cart == null)
+            {
+                return NotFound();
+            }
+
+            var cart = await _context.Cart
+                .Include(c => c.Order)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cart == null)
+            {
+                return NotFound();
+            }
+
+            return View(cart);
+        }
+
+        // POST: Cart/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Cart == null)
+            {
+                return Problem("Entity set 'FPTDBTContext.Cart'  is null.");
+            }
+            var cart = await _context.Cart.FindAsync(id);
+            if (cart != null)
+            {
+                _context.Cart.Remove(cart);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CartExists(int id)
+        {
+          return (_context.Cart?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
